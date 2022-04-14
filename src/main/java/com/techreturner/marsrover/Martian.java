ba1ebf0ccf750f086b33fdc0267c90421dc88db9
Fung Chow <fungchow@gmail.com>
2022-04-14 23:10:30 +0100

    add class Martian

--- src/main/java/com/techreturner/marsrover/Martian.java
+++ src/main/java/com/techreturner/marsrover/Martian.java
@@ -1,17 +1,36 @@
 package com.techreturner.marsrover;
 
-public class Martian {
+import java.util.ArrayList;
 
-    public static void main(String[] args) {
-        Martian a = new Martian();
-        System.out.println(a.getRandomInt(0, 1));
-        System.out.println();
+public class Martian extends Movable{
 
+
+    public Martian(Mars planet){
+        this.planet = planet;
     }
 
     public int getRandomInt(int min, int max) {
-
         return (int) Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+
+    @Override
+    public String moveOnMars(char action) {
+        //Martian moves diagonally and randomly
+        int moves = 2;
+        for (int i =0; i < moves; i++){
+            if (getRandomInt(0,1) == 0)
+                action('L');
+            else
+                action('R');
+            action('M');
+        }
+        return getPositionDirection();
+    }
+
+    @Override
+    public void eventListener(String event, ArrayList<Integer> involved) {
+        System.out.println("Martian has heard an event.");
     }
 }
 
