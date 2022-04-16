864971ce7f0049e135acb327751af49bdaa1a470
Fung Chow <fungchow@gmail.com>
2022-04-16 22:59:50 +0100

    Mars Class add  method to set the size of the grid and print out the grid.

--- src/main/java/com/techreturner/marsrover/Mars.java
+++ src/main/java/com/techreturner/marsrover/Mars.java
@@ -1,6 +1,9 @@
 package com.techreturner.marsrover;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Mars {
 
     private final int MINCOLUMNNO = 0;
@@ -8,6 +11,7 @@ public class Mars {
     private final String MOVEMENTSTRING = "LRM";
     private int upperRightCornerCol = 5;
     private int upperRightCornerRoW = 5;
+    private ArrayList<ArrayList<String>> planetGrid = new ArrayList<ArrayList<String>>();
 
     private void promptUserInput() {}
 
@@ -15,6 +19,43 @@ public class Mars {
     public int getMAXCOLUMNNO(){return MAXCOLUMNNO;};
     public int getUpperRightCornerCol(){return upperRightCornerCol;}
     public int getUpperRightCornerRoW(){return upperRightCornerRoW;}
+    public ArrayList<ArrayList<String>> getGrid(){return planetGrid;};
+    public void setGrid(String grid){
+        String[] gridArray = grid.split(" ");
+        upperRightCornerRoW = Integer.valueOf(gridArray[0]);
+        upperRightCornerCol = Integer.valueOf(gridArray[1]);
+        for (int i=0; i<= upperRightCornerRoW; i++) {
+            planetGrid.add(new ArrayList<>());
+            for (int j = 0; j <= upperRightCornerCol; j++)
+                planetGrid.get(planetGrid.size() - 1).add(" ");
+        }
+    }
 
+    public String printGrid(){
+
+
+        String gridLine = "+";
+        for (int i=0; i<= upperRightCornerCol; i++)
+            gridLine += "-+";
+        String grid = gridLine + "\n";
+
+        for (int i=0; i <= upperRightCornerRoW; i++){
+            grid += "|";
+            for (int j=0; j <= upperRightCornerCol; j++)
+                grid += planetGrid.get(i).get(j) + "|";
+            grid += "\n" + gridLine + "\n";
+        }
+        System.out.println(grid);
+
+        return "hello world"; //grid;
+    }
+
+    public static void main(String[] args) {
+        Mars a = new Mars();
+        a.setGrid("55 35");
+        System.out.println("row size : " + a.getGrid().size());
+        System.out.println("col size : " + a.getGrid().get(1).size());
+        a.printGrid();
+    }
 
 }
---
+++ src/test/java/com/techreturner/marsrover/MarsTest.java
@@ -0,0 +1,29 @@
+package com.techreturner.marsrover;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MarsTest {
+
+    private Mars planet = new Mars();
+
+    @Test
+    public void checkMarsGridTest (){
+        planet.setGrid("5 5");
+        assertEquals(6, planet.getGrid().size());
+        assertEquals(6, planet.getGrid().get(0).size());
+        assertEquals(6, planet.getGrid().get(1).size());
+        assertEquals(6, planet.getGrid().get(2).size());
+        assertEquals(6, planet.getGrid().get(3).size());
+        assertEquals(6, planet.getGrid().get(4).size());
+        assertEquals(6, planet.getGrid().get(5).size());
+    }
+
+
+    public void printGrid(){
+        planet.printGrid();
+    }
+
+
+}
